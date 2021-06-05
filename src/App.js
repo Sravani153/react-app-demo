41d93e6f53b2ae2a95983049bf0d33ba8bbda148
Modugaparapu Sravani <68905559+Sravani153@users.noreply.github.com>
2021-06-05 17:59:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import {Navbar, NavbarBrand, NavLink} from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import { Navbar, NavbarBrand } from "reactstrap";
 
- function App() {
-  return (
-    <div>
-      <Navbar dark  color="success">
-        <div className="container">
-          <NavbarBrand href="/">Droid Cafe</NavbarBrand>
-          <NavLink href="/home" className="active">Home</NavLink>
-          <NavLink href="/about">About this</NavLink>
-        </div>
-      </Navbar>
-       <Menu />  
-    </div>
-  );
+import Menu from './components/MenuComponent';
+
+import './App.css';
+import { DISHES } from './shared/dishes'
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  } 
+
+  render(){
+
+    return(
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu dishes={ this.state.dishes } />
+      </div>
+    );
+
+  }
+
 }
+
 export default App;
